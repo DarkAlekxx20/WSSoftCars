3bfc9c3844cd3c925db7616b76f257b26a879651
DarkAlekxx20 <niko.beelik@hotmail.com>
2024-10-18 19:17:00 -0600

    Se modifica servicio REST de usuarios agregando micro-service para acceder a la aplicacion

--- src/java/com/softcars/mx/services/UsuarioREST.java
+++ src/java/com/softcars/mx/services/UsuarioREST.java
@@ -3,6 +3,7 @@ package com.softcars.mx.services;
 import com.google.gson.Gson;
 import com.softcars.mx.core.UsuarioController;
 import com.softcars.mx.model.Usuario;
+import jakarta.ws.rs.DefaultValue;
 import jakarta.ws.rs.FormParam;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
@@ -30,6 +31,31 @@ public class UsuarioREST {
             u = gson.fromJson(userData, Usuario.class);
             uc.sigin(u);
             out = "{\"Success\":\"Se guardo correctamente el usuario!\"}\n";
+        } catch (Exception e) {
+            e.printStackTrace();
+            out = "{\"error\":\"Error en base de datos :(\"}\n";
+        }
+        return Response.status(Response.Status.OK).entity(out).build();
+    }
+
+    @Path("login")
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response login(@FormParam("userData") @DefaultValue("") String userData) throws Exception {
+        String out = null;
+        Gson gson = new Gson();
+        Usuario u = null;
+        UsuarioController uc = new UsuarioController();
+        try {
+            u = gson.fromJson(userData, Usuario.class);
+            u = uc.login(u.getUsername(), u.getPassword());
+            if (u != null && u.getEstatus() == 1) {
+                out = gson.toJson(u);
+            } else if (u.getEstatus() != 1) {
+                out = "{\"error\":\"El usuario esta con estatus inactivo!\"}\n";
+            } else {
+                out = "{\"error\":\"Datos de credencial incorrectos!\"}\n";
+            }
         } catch (Exception e) {
             e.printStackTrace();
             out = "{\"error\":\"Error en base de datos :(\"}\n";
